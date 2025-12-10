e300493f0c981327f1890cd78f04d29fd647b6e3
Maksim_Dimitrak <max.dimitrak@gmail.com>
2025-12-09 17:31:50 -0800

    improve solution for return negative (8kyu)

--- 8kyu/Return_Negative.js
+++ 8kyu/Return_Negative.js
@@ -12,9 +12,6 @@
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
 function makeNegative(num) {
-  if (num <= 0){
-    return num;
-  }else{
-    return -num;
-  }
+ return num < 0 ? num : -num;
 }
+
